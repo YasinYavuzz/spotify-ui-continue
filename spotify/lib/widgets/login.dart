27d2031dee3c9c5bfba8254c5acde549a7a4a1cc
Yasin Yavuz <yavuzyasin.eem@gmail.com>
2022-09-21 14:18:02 +0300

    add getstarted

---
+++ spotify/lib/widgets/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({super.key});
+
+  @override
+  State<LoginWidget> createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        
+        
+
+
+
+
+    );
+  }
+}
